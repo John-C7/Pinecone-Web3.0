63bc12f125e75a1a411c1ff50a7f01c736ab4a6f
Madhav Mundhra <madhavmundhra221@gmail.com>
2024-05-18 13:29:21 +0530

    Added Login

--- frontend/carbon/src/App.js
+++ frontend/carbon/src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
+import { LoginPage } from "./components/Login/LoginPage";
+import { RegisterPage } from "./components/Register/RegisterPage";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <RegisterPage/>
+    </>
+  )
 }
---
+++ frontend/carbon/src/components/Register/RegisterForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './RegisterForm.css'
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+export const RegisterForm = () => {
+  return (
+    <div className='mainpage'>
+        <div className='wrapper'>
+            <form action="">
+                <h1>Register</h1>
+                <div className='input-box'>
+                    <input type="text" placeholder='Username' required />
+                    <FaUser className='icon'/>
+                </div>
+                <div className='input-box'>
+                    <input type="email" placeholder='E-mail' required />
+                    <MdEmail className='icon'/>
+                </div>
+                <div className='input-box'>
+                    <input type="password" placeholder='New Password' required />
+                    <FaLock className='icon'/>
+                </div>
+
+                <div className='input-box'>
+                    <input type="password" placeholder='Confirm Password' required />
+                    <FaLock className='icon'/>
+                </div>
+                <button type='submit'>Sign Up</button>
+            </form>
+        </div>
+    </div>
+  );
+};
